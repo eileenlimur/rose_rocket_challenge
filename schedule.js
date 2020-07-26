34d5464e3acc66a72c97f6fbaa24f80484d456b5
Eileen Li <eileenlimur@gmail.com>
2020-07-26 15:40:54 -0400

    create schedule.js with object containing hard-coded driver schedule

---
+++ schedule.js
@@ -0,0 +1,24 @@
+const drivers = {
+  "Fierce Bob": {
+    "Tuesday": {
+      02: {
+        "hours": 2,
+        "task": "Pickup: Eaton Centre"
+      },
+      04: {
+        "hours": 1,
+        "task": "Dropoff: Ripley's Aquarium"
+      }
+    }
+  },
+  "Tough Nancy": {
+    "Monday": {
+      12: {
+        "hours": 1,
+        "task": "Pickup: Toronto Zoo"
+      }
+    }
+  }
+}
+
+export default drivers;
